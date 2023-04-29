2df2514a4efabebaac8fd73d52b81673b9d9a349
fred <domaine75@gmail.com>
2023-04-30 00:12:16 +0200

    Clean code

--- HelloWorld/WindowsFormsApplication1/FormMain.cs
+++ HelloWorld/WindowsFormsApplication1/FormMain.cs
@@ -27,20 +27,20 @@
 
 namespace WindowsFormsApplication1
 {
-    using System;
-    using System.Windows.Forms;
+  using System;
+  using System.Windows.Forms;
 
-    using OxyPlot;
-    using OxyPlot.Series;
+  using OxyPlot;
+  using OxyPlot.Series;
 
-    public partial class Form1 : Form
+  public partial class FormMain : Form
+  {
+    public FormMain()
     {
-        public Form1()
-        {
-            this.InitializeComponent();
-            var myModel = new PlotModel { Title = "Example 1" };
-            myModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
-            this.plot1.Model = myModel;
-        }
+      InitializeComponent();
+      var myModel = new PlotModel { Title = "Example 1" };
+      myModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+      plot1.Model = myModel;
     }
+  }
 }
